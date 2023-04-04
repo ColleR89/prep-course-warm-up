53af55eb0036ee6750f920f728f4989580aa00a1
Andrejs Lavrinovics <andrew.lavrinovich@inbox.lv>
2023-04-04 11:50:34 +0000

    26 - done

--- src/26-validation.ts
+++ src/26-validation.ts
@@ -11,7 +11,13 @@ export {};
 const excludedNums = [6, 14, 91, 111];
 
 // You are allowed to edit only this function
-function validate(num) {}
+function validate(num: any): boolean {
+  if (num === +num && Number.isInteger(num) && excludedNums.indexOf(num)) {
+    return true;
+  } else {
+    return false;
+  }
+}
 
 console.log(validate(6));
 console.log(validate(10.5));
