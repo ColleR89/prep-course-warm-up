540d63d0393a9925b9c30c65e45573d26667c8a9
Andrejs Lavrinovics <andrew.lavrinovich@inbox.lv>
2023-04-04 11:25:56 +0000

    23 - done

--- src/23-min-max.ts
+++ src/23-min-max.ts
@@ -5,11 +5,38 @@ export {};
  * one of which does not use built-in Math methods.
  */
 
-const min = array => {};
+function min(arr: number[]): number {
+  return Math.min(...arr); //Kas par ... ?
+}
 
-const max = array => {};
+function max(arr: number[]): number {
+  return Math.max(...arr);
+}
+
+function min2(arr: number[]): number {
+    let i: number;
+    let min: number = 99999999999999999;
+    for (i = 0; i < arr.length; i++) {
+        arr[i] < min ? min = arr[i] : min = min;
+    }
+    return min;
+}
+
+function max2(arr: number[]): number {
+    let i: number;
+    let max: number = -99999999999999999;
+    for (i = 0; i < arr.length; i++) {
+        arr[i] > max ? max = arr[i] : max = max;
+    }
+    return max;
+}
 
 console.log(min([1, 2, 3, 4, 5])); // Expected output: 1
 console.log(min([9, -3, 6])); // Expected output: -3
 console.log(max([1, 2, 3, 4, 5])); // Expected output: 5
 console.log(max([9, -3, 6])); // Expected output: 9
+
+console.log(min2([1, 2, 3, 4, 5])); // Expected output: 1
+console.log(min2([9, -3, 6])); // Expected output: -3
+console.log(max2([1, 2, 3, 4, 5])); // Expected output: 5
+console.log(max2([9, -3, 6])); // Expected output: 9
