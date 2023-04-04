677317c825633b6e8fa98e38089dafa9117866aa
Andrejs Lavrinovics <andrew.lavrinovich@inbox.lv>
2023-04-04 10:15:52 +0000

    19 - done

--- src/19-currency-conversion.ts
+++ src/19-currency-conversion.ts
@@ -15,14 +15,31 @@ export {};
  */
 
 // You are allowed to change this function
-function convertToUSD(price) {}
+/*function convertToUSD(num: number): number {
+  num = +(num * 1.25).toFixed(2);
+  num = +(num * 1.01).toFixed(2); //1% fee
+  return num;
+}*/
 // You are allowed to change this function
-function convertToBRL(price) {}
+/*function convertToBRL(num: number): number {
+  num = +(num * 6.33).toFixed(2);
+  num = +(num * 1.01).toFixed(2); //1% fee
+  return num;
+}*/
+// Konversijas funkcijs, gan USD, gan BRL
+function convertTo(num: number, to: string): number {
+  let coef: number;
+  let fee = 0.01; //1%
+  coef = to === "USD" ? 1.25 : 6.33;
+  num = +(num * coef).toFixed(2);
+  num += +(num * fee).toFixed(2); //1% fee
+  return +num.toFixed(2);
+}
 
 const product = "You don't know JS";
 const price = 12.5;
-const priceInUSD = convertToUSD(price);
-const priceInBRL = convertToBRL(price);
+const priceInUSD = convertTo(price, "USD");
+const priceInBRL = convertTo(price, "BRL");
 
 console.log("Product: " + product);
 console.log("Price: $" + priceInUSD);
