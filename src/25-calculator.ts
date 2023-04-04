2229498b42bfd80846ecffc0b7e2fc23428248f8
Andrejs Lavrinovics <andrew.lavrinovich@inbox.lv>
2023-04-04 11:42:05 +0000

    25 - done

--- src/25-calculator.ts
+++ src/25-calculator.ts
@@ -1,14 +1,34 @@
 export {};
 
-function add() {}
+function add(a: number, b: number): number {
+  return a + b;
+}
 
-function subtract() {}
+function subtract(a: number, b: number): number {
+  return a - b;
+}
 
-function sum() {}
+function sum(nums: number[]): number {
+  let i: number;
+  let sum: number = 0;
+  for (i = 0; i < nums.length; i++) {
+    sum += nums[i];
+  }
+  return sum;
+}
 
-function multiply() {}
+function multiply(nums: number[]): number {
+  let i: number;
+  let mult: number = 1;
+  for (i = 0; i < nums.length; i++) {
+    mult *= nums[i];
+  }
+  return mult;
+}
 
-function power() {}
+function power(a: number, b: number): number {
+  return Math.pow(a, b);
+}
 
 console.log(add(1, 2)); // Expected output: 3
 console.log(subtract(1, 2)); // Expected output: -1
