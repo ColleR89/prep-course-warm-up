68097a2cd51865469a433028eaca038793c45e5f
Andrejs Lavrinovics <andrew.lavrinovich@inbox.lv>
2023-04-04 10:41:23 +0000

    21 - done

--- src/21-map.ts
+++ src/21-map.ts
@@ -1,16 +1,24 @@
 export {};
 
-function tidyUpString(str) {
-  return str
-    .trim()
-    .toLowerCase()
-    .replace("/", "");
+function tidyUpString(str: string[]): string[] {
+  let i: number;
+  for (i = 0; i < str.length; i++) {
+    str[i] = str[i].trim().toLowerCase().replace("/", "");
+  }
+  return str;
+  //return str.trim().toLowerCase().replace("/", "");
 }
 
 // You are allowed to edit this function
-function capitalise(str) {}
+function capitalise(str: string[]): string[] {
+  let i: number;
+  for (i = 0; i < str.length; i++) {
+    str[i] = str[i][0].toUpperCase() + str[i].slice(1);
+  }
+  return str;
+}
 
 const mentors = ["/Daniel ", "irina ", " Gordon", "ashleigh "];
-let mentorsTidy; // You are allowed to edit this line
+let mentorsTidy: string[] = capitalise(tidyUpString(mentors)); // You are allowed to edit this line
 
 console.log(mentorsTidy); // Expected output: ["Daniel", "Irina", "Gordon", "Ashleigh"]
