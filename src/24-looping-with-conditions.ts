633ea4452b6b9e5be5fc1e4346c870e6ea87427d
Andrejs Lavrinovics <andrew.lavrinovich@inbox.lv>
2023-04-04 11:34:34 +0000

    24 - done

--- src/24-looping-with-conditions.ts
+++ src/24-looping-with-conditions.ts
@@ -1,5 +1,14 @@
 export {};
 
-function onlyTheAces(arr) {}
+function onlyTheAces(arr: string[]): string[] {
+  let arr2: string[] = Array();
+  let i: number;
+  for (i = 0; i < arr.length; i++) {
+    if (arr[i] === "Ace") {
+      arr2.push(arr[i]);
+    }
+  }
+  return arr2;
+}
 
 console.log(onlyTheAces(["Ace", "King", "Queen", "Jack", "Ace"])); // Expected result: ['Ace', 'Ace']
