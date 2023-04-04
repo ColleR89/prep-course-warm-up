87819cb29b6d2a679b3df5f99b89d3c65c78afe0
Andrejs Lavrinovics <andrew.lavrinovich@inbox.lv>
2023-04-04 07:14:45 +0000

    14 - done

--- src/14-repeat-string.ts
+++ src/14-repeat-string.ts
@@ -1,6 +1,13 @@
 export {};
 
-const repeatString = () => {};
+const repeatString = (what: string, cnt: number): string => {
+  let i: number;
+  let str: string = "";
+  for (i = 0; i < cnt; i++) {
+    str = str.concat(what);
+  }
+  return str;
+};
 
 console.log(repeatString("a", 4)); // Expected output: 'aaaa'
 console.log(repeatString("b", 5)); // Expected output: 'bbbbb'
