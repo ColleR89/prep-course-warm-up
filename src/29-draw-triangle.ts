2d9390cdf44cb308142671b77b251e8f8269bfb6
Andrejs Lavrinovics <andrew.lavrinovich@inbox.lv>
2023-04-04 13:09:37 +0000

    29 - done

--- src/29-draw-triangle.ts
+++ src/29-draw-triangle.ts
@@ -5,7 +5,12 @@ export {};
  *  - https://blog.codeanalogies.com/2017/11/07/javascript-for-loops-explained/
  */
 
-function draw() {}
+function draw(cnt: number) {
+  let i: number;
+  for (i = 0; i < cnt + 1; i++) {
+    console.log("*".repeat(i));
+  }
+}
 
 draw(3);
 /* Expected output:
