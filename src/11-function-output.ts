c223d33f850ff5cae79214774304c9672f92b567
Andrejs Lavrinovics <andrew.lavrinovich@inbox.lv>
2023-04-04 06:55:24 +0000

    09, 10, 11, 12 - done

--- src/11-function-output.ts
+++ src/11-function-output.ts
@@ -1,7 +1,12 @@
 export {};
 
-function concatenate(firstWord, secondWord, thirdWord) {
+function concatenate(
+  firstWord: string,
+  secondWord: string,
+  thirdWord: string
+): string {
   // Write the body of this function to concatenate three words together with space between them
+  return firstWord.concat(" ").concat(secondWord).concat(" ").concat(thirdWord);
 }
 
 const result = concatenate("Hello", "from", "CODELEX");
