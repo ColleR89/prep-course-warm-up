1003b47896cca71b8c303739508431f1fe2c54a8
Andrejs Lavrinovics <andrew.lavrinovich@inbox.lv>
2023-04-04 07:10:20 +0000

    13 - done

--- src/13-is-blank.ts
+++ src/13-is-blank.ts
@@ -4,6 +4,17 @@ export {};
  * Create a function called isBlank, which checks if passed string is blank or not
  */
 
+function isBlank(inp: any): boolean {
+  if (inp == null) {
+    return true;
+  }
+  if (inp != "" && inp != " ") {
+    return false;
+  } else {
+    return true;
+  }
+}
+
 console.log(isBlank(null)); // Expected output: true
 console.log(isBlank("")); // Expected output: true
 console.log(isBlank(" ")); // Expected output: true
