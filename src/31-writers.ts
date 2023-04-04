f8157d787c446131b0a6b8d03a88bcd7c112a436
Andrejs Lavrinovics <andrew.lavrinovich@inbox.lv>
2023-04-04 13:24:00 +0000

    31- done

--- src/31-writers.ts
+++ src/31-writers.ts
@@ -11,27 +11,36 @@ const writers = [
     lastName: "Woolf",
     occupation: "writer",
     age: 59,
-    alive: false
+    alive: false,
   },
   {
     firstName: "Zadie",
     lastName: "Smith",
     occupation: "writer",
     age: 41,
-    alive: true
+    alive: true,
   },
   {
     firstName: "Jane",
     lastName: "Austen",
     occupation: "writer",
     age: 41,
-    alive: false
+    alive: false,
   },
   {
     firstName: "bell",
     lastName: "hooks",
     occupation: "writer",
     age: 64,
-    alive: true
-  }
+    alive: true,
+  },
 ];
+
+let i: number;
+for (i = 0; i < writers.length; i++) {
+  if (writers[i].alive) {
+    console.log(
+      `Hi, my name is ${writers[i].firstName} ${writers[i].lastName}. I am ${writers[i].age} years old, and work as a ${writers[i].occupation}.`
+    );
+  }
+}
