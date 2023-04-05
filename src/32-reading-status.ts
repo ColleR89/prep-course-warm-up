bc17249547fb7299ee2e96324b5e7bd08634f14f
Andrejs Lavrinovics <andrew.lavrinovich@inbox.lv>
2023-04-05 05:27:50 +0000

    32 - done

--- src/32-reading-status.ts
+++ src/32-reading-status.ts
@@ -4,21 +4,34 @@ const library = [
   {
     title: "Bill Gates",
     author: "The Road Ahead",
-    isRead: true
+    isRead: true,
   },
   {
     title: "Steve Jobs",
     author: "Walter Isaacson",
-    isRead: true
+    isRead: true,
   },
   {
     title: "Mockingjay: The Final Book of The Hunger Games",
     author: "Suzanne Collins",
-    isRead: false
-  }
+    isRead: false,
+  },
 ];
 
-const showStatus = () => {};
+const showStatus = (books: any) => {
+  let i: number;
+  let titles: string[] = Array();
+  for (i = 0; i < books.length; i++) {
+    if (books[i].isRead) {
+      console.log(`Already read '${books[i].title}' by ${books[i].author}.`);
+    } else {
+      console.log(
+        `You still need to read '${books[i].title}' by ${books[i].author}.`
+      );
+    }
+    titles[i] = books[i].title;
+  }
+};
 
 showStatus(library);
 
