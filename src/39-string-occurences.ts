74f86e5b9818071412300642cb091c0f3a0378f5
Andrejs Lavrinovics <andrew.lavrinovich@inbox.lv>
2023-04-05 07:43:41 +0000

    39 - done

--- src/39-string-occurences.ts
+++ src/39-string-occurences.ts
@@ -1,6 +1,8 @@
 export {};
 
-const count = () => {};
+const count = (str: string, what: string): number => {
+  return str.toLowerCase().split(what.toLowerCase()).length - 1;
+};
 
 console.log(count("The quick brown fox jumps over the lazy dog", "the")); // Expected output: 2
 console.log(count("The quick brown fox jumps over the lazy dog", "fox")); // Expected output: 1
