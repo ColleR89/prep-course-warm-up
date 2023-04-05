2aca04e121ba970497c7bce5464d32c9a3511b03
Andrejs Lavrinovics <andrew.lavrinovich@inbox.lv>
2023-04-05 05:38:42 +0000

    33 - done

--- src/33-temperature-conversion.ts
+++ src/33-temperature-conversion.ts
@@ -14,8 +14,12 @@ export {};
  * Temperature should be rounded to one decimal place: i.e., fahrenheitToCelsius(100) should return 37.8 and not 37.77777777777778.
  */
 
-const fahrenheitToCelsius = ?
-const celsiusToFahrenheit = ?
+const fahrenheitToCelsius = (inp: number): number => {
+  return Math.round((inp - 32) * (5 / 9) * 10) / 10;
+};
+const celsiusToFahrenheit = (inp: number): number => {
+  return Math.round((inp * (5 / 9) + 32) * 10) / 10;
+};
 
 console.log(fahrenheitToCelsius(32)); // Expected result: 0
 console.log(celsiusToFahrenheit(0)); // Expected result: 32
